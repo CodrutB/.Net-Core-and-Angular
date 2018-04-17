5c2f61a5c58a00cde40344fe58bede798f5fef47
Codrut <codrut.burdujoc@yahoo.ro>
2018-04-16 22:33:05 -0400

    Adding first bower confing file and js scripts.

---
+++ CoreAngularPoC/wwwroot/js/index.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("buying item.")
+    });
+
+    var productsInfo = $(".product-props li");
+    productsInfo.on("click", function () {
+        console.log($(this).text());
+    });
+
+    var loginToggle = $("#loginToggle");
+    var popupForm = $(".popupForm");
+
+    loginToggle.on("click", function () {
+        popupForm.fadeToggle(1000);
+    });
+});
