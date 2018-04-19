db18191d1face8c5588bf2bb14276339d03fb9a7
Codrut <codrut.burdujoc@yahoo.ro>
2018-04-19 19:58:17 -0400

    Adding first MVC elements

---
+++ CoreAngularPoC/Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreAngularPoC.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
